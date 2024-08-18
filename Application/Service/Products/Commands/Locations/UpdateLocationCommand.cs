e3787ff8fa5a16f8078660eb48165bef8e185a3e
Aysegul <karadan.ay7@gmail.com>
2024-08-18 17:20:08 +0300

    Location commands added

---
+++ Application/Service/Products/Commands/Locations/UpdateLocationCommand.cs
@@ -0,0 +1,10 @@
+using System;
+using Application.DTO.Request.Products;
+using Application.DTO.Response;
+using MediatR;
+
+namespace Application.Service.Products.Commands.Locations;
+
+public record UpdateLocationCommand(UpdateLocationRequestDTO LocationModel):IRequest<ServiceResponse>;
+ 
+
--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abce3bda99f211e926fa2f4fc9bee8c42cf27219")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffba123c0772e755b8b913f0e387b8909ccae385")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
