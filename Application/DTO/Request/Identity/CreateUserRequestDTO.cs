99ed940cead9c5401dea785d0140e7f8f2adb8ab
Aysegul <karadan.ay7@gmail.com>
2024-08-20 14:11:17 +0300

    Add user page updated and regular exp added

--- Application/DTO/Request/Identity/CreateUserRequestDTO.cs
+++ Application/DTO/Request/Identity/CreateUserRequestDTO.cs
@@ -5,11 +5,20 @@ namespace Application.DTO.Request.Identity;
 
 public class CreateUserRequestDTO :LoginUserRequestDTO
 {
-  [Required]
-  public string Name { get; set; }
-    [Required]
-    [Compare(nameof(Password))]
-    public string ConfirmPassword { get; set; }
-    [Required]
-    public string Policy { get; set; }
+   
+       [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 20 characters.")]
+        [RegularExpression(@"^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[a-zA-Z\d@$!%*?&]*$", 
+            ErrorMessage = "Password must include at least one special character.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required.")]
+        [Compare(nameof(Password), ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Policy is required.")]
+        public string Policy { get; set; }
 }
