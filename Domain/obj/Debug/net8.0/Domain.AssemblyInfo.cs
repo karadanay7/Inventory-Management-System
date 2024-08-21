525ac0d7458ed14eb21a6f6971eed9ff51c30e8c
Aysegul <karadan.ay7@gmail.com>
2024-08-21 15:06:36 +0300

    laout changed and sidebar is resposive now

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66db4825aee6c6d3f9d5d4a52c82367ddcf64d2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7607a9266d53a5a48c6c11c13f7051335de726e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
